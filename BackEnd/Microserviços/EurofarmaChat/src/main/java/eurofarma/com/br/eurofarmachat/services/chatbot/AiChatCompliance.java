759414e97334f685ae61ab9fb4c500e448db717a
Igor Pasqualino <igorpasqualino@gmail.com>
2024-04-15 11:22:35 -0300

    Ajuste no prompt de compliance

--- BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/services/chatbot/AiChatCompliance.java
+++ BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/services/chatbot/AiChatCompliance.java
@@ -4,6 +4,9 @@ import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface AiChatCompliance extends AiChat {
-    @SystemMessage("Você é um chatbot de compliance da Eurofarma, você responde apenas em português.")
+    @SystemMessage("Você é um assistente de inteligência artificial especializado" +
+            " em compliance da Eurofarma. Sua principal função é responder perguntas relacionadas" +
+            " ao compliance. Você deve se comunicar exclusivamente em português. Você deve recusar" +
+            " perguntas que não estejam relacionadas ao compliance. Seu objetivo é fornecer informações precisas e úteis sobre compliance.")
     String chat(String userMessage);
 }
--- BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/services/chatbot/Implements/ChatBotCompliance.java
+++ BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/services/chatbot/Implements/ChatBotCompliance.java
@@ -3,7 +3,7 @@ import dev.langchain4j.service.AiServices;
 import eurofarma.com.br.eurofarmachat.factorys.ChatLanguageModelFactory;
 import eurofarma.com.br.eurofarmachat.factorys.EmbeddingStoreContentRetrieverFactory;
 import eurofarma.com.br.eurofarmachat.services.chatbot.AiChat;
-import eurofarma.com.br.eurofarmachat.services.chatbot.AichatEuroFarma;
+import eurofarma.com.br.eurofarmachat.services.chatbot.AiChatCompliance;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,7 +14,7 @@ public class ChatBotCompliance implements AiChat {
     ChatLanguageModelFactory chatLanguageModelFactory = new ChatLanguageModelFactory();
 
     public ChatBotCompliance(){
-        this.aiChatCompliance = AiServices.builder(AichatEuroFarma.class)
+        this.aiChatCompliance = AiServices.builder(AiChatCompliance.class)
                 .chatLanguageModel(chatLanguageModelFactory.chatLanguageModelOllama())
                 .contentRetriever(embeddingStoreFactory.embeddingStoreContentRetriever("eurocompliance"))
                 .build();
@@ -22,15 +22,8 @@ public class ChatBotCompliance implements AiChat {
 
     @Override
     public String chat(String pergunta) {
-        if (isComplianceRelated(pergunta)) {
             return aiChatCompliance.chat(pergunta);
-        } else {
-            return "Não tenho essas informações.";
-        }
+
     }
 
-    private boolean isComplianceRelated(String pergunta) {
-        String resposta = chatLanguageModelFactory.chatLanguageModelOllama().generate("Responda com sim ou não essa pergunta tem haver com compliance:" + pergunta);
-        return resposta.contains("Sim") || resposta.contains("sim") ;
-    }
 }
