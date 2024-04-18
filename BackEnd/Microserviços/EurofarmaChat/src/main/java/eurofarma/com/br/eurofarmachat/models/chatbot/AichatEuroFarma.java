ea57485ada9686f8d37b4767455e9b44429a7a50
Igor Pasqualino <igorpasqualino@gmail.com>
2024-04-18 19:39:13 -0300

    Change the prompt of EuroData

--- BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/models/chatbot/AichatEuroFarma.java
+++ BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/models/chatbot/AichatEuroFarma.java
@@ -6,6 +6,6 @@ import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface AichatEuroFarma extends AiChat {
-    @SystemMessage("Você é um chatbot da Eurofarma, você responde apenas em português.Devolva trechos dos texto contendo essas informações")
+    @SystemMessage("Você é um chatbot especializado em busca de informação da eurofarma.Você deve se comunicar exclusivamente em português.Entregue fragmentos dos texto que sobre a pergunta")
     String chat(String userMessage);
 }
