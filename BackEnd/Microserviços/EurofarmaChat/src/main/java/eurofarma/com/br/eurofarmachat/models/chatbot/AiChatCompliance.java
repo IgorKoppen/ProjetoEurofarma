342da937c57ac8b8e8c0e230f55a97943d42d22c
Igor Pasqualino <igorpasqualino@gmail.com>
2024-04-19 10:17:02 -0300

    Change into the prompt

--- BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/models/chatbot/AiChatCompliance.java
+++ BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/models/chatbot/AiChatCompliance.java
@@ -5,6 +5,7 @@ import dev.langchain4j.service.spring.AiService;
 @AiService
 public interface AiChatCompliance extends AiChat {
     @SystemMessage("Você é um Chatbot especializado" +
-            " em compliance da Eurofarma. Você deve se comunicar exclusivamente em português.Entregue fragmentos dos texto que sobre a pergunta")
+            " em compliance da Eurofarma. Você deve se comunicar exclusivamente" +
+            " em português e responder sobre compliance.Entregue fragmentos dos texto que sobre a pergunta")
     String chat(String userMessage);
 }
--- BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/models/chatbot/AichatEuroFarma.java
+++ BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/models/chatbot/AichatEuroFarma.java
@@ -7,7 +7,7 @@ import dev.langchain4j.service.spring.AiService;
 @AiService
 public interface AichatEuroFarma extends AiChat {
     @SystemMessage("Você é um funcionario da eurofarma especializado em busca de informação da eurofarma." +
-            "Você deve se comunicar exclusivamente em português." +
+            "Você deve se comunicar exclusivamente em português e fazer sentido com o contexto da eurofarma." +
             "Entregue os fragmentos de texto relevantes e um resumo sobre o assunto.")
     String chat(String userMessage);
 }
