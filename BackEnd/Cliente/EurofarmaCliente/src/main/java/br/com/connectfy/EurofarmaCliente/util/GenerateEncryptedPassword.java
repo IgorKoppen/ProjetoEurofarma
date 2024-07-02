868de7532c17f7c834810f37dcb16d2034e7b7ed
Igor Pasqualino <igorpasqualino@gmail.com>
2024-07-01 22:44:11 -0300

    Encriptação da password

---
+++ BackEnd/Cliente/EurofarmaCliente/src/main/java/br/com/connectfy/EurofarmaCliente/util/GenerateEncryptedPassword.java
@@ -0,0 +1,24 @@
+package br.com.connectfy.EurofarmaCliente.util;
+
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenerateEncryptedPassword {
+    private static final Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 16, 310000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+    private static final DelegatingPasswordEncoder passwordEncoder;
+
+    static  {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+    }
+
+    public static String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+}
