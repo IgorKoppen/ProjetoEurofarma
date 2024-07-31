64506cb6e59de3655c47e8c3fb44d4fcb1acdec3
Igor Pasqualino <igorpasqualino@gmail.com>
2024-07-31 19:46:44 -0300

    remoção do IA com javaLangChainJ

--- BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/dtos/AnswerDTO.java
+++
@@ -1,7 +0,0 @@
-package eurofarma.com.br.eurofarmachat.dtos;
-
-
-public record AnswerDTO(
-        String question,
-        String answer) {
-}
--- BackEnd/Microserviços/EurofarmaChat/src/main/java/eurofarma/com/br/eurofarmachat/models/chatbot/AiChat.java
+++
@@ -1,7 +0,0 @@
-package eurofarma.com.br.eurofarmachat.models.chatbot;
-
-
-public interface AiChat {
-
-     String chat(String message);
-}
