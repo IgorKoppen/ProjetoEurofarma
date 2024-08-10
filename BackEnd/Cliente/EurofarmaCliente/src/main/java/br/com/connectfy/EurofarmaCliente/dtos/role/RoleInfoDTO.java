3a9e2c5c002d41a509430efd7745bbbb3be56633
Igor Pasqualino <igorpasqualino@gmail.com>
2024-08-09 22:34:54 -0300

    Refactored RoleInfo

--- BackEnd/Cliente/EurofarmaCliente/src/main/java/br/com/connectfy/EurofarmaCliente/dtos/role/RoleInfoDTO.java
+++ BackEnd/Cliente/EurofarmaCliente/src/main/java/br/com/connectfy/EurofarmaCliente/dtos/role/RoleInfoDTO.java
@@ -1,11 +1,13 @@
 package br.com.connectfy.EurofarmaCliente.dtos.role;
 
 import br.com.connectfy.EurofarmaCliente.models.Role;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import java.io.Serial;
 import java.io.Serializable;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RoleInfoDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
