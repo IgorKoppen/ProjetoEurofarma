c5d9942593c3a9b9585008e8854d97f19db90f50
Igor Pasqualino <igorpasqualino@gmail.com>
2024-08-22 00:29:12 -0300

    Fix: removed http status

--- BackEnd/Cliente/EurofarmaCliente/src/main/java/br/com/connectfy/EurofarmaCliente/exceptions/IncorrectPasswordException.java
+++ BackEnd/Cliente/EurofarmaCliente/src/main/java/br/com/connectfy/EurofarmaCliente/exceptions/IncorrectPasswordException.java
@@ -1,6 +1,10 @@
 package br.com.connectfy.EurofarmaCliente.exceptions;
 
+import java.io.Serial;
+
 public class IncorrectPasswordException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
     public IncorrectPasswordException(String message) {
         super(message);
     }
