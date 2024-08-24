b55a02e487a42256b03bc25b904380d80f4d6087
Igor Pasqualino <igorpasqualino@gmail.com>
2024-08-24 19:23:38 -0300

    Add employeeRegistration

--- BackEnd/Cliente/EurofarmaCliente/src/main/java/br/com/connectfy/EurofarmaCliente/dtos/instructor/InstructorInfo.java
+++ BackEnd/Cliente/EurofarmaCliente/src/main/java/br/com/connectfy/EurofarmaCliente/dtos/instructor/InstructorInfo.java
@@ -7,12 +7,14 @@ public class InstructorInfo {
     private final String name;
     private final String surname;
     private final String cellphoneNumber;
+    private final Long employeeRegistration;
 
     public InstructorInfo(Instructor entity) {
         this.id = entity.getId();
         this.name = entity.getEmployee().getName();
         this.surname = entity.getEmployee().getSurname();
         this.cellphoneNumber = entity.getEmployee().getCellphoneNumber();
+        this.employeeRegistration = entity.getEmployee().getEmployeeRegistration();
     }
 
     public Long getId() {
@@ -29,5 +31,9 @@ public class InstructorInfo {
 
     public String getCellphoneNumber() {
         return cellphoneNumber;
+    }
+
+    public Long getEmployeeRegistration() {
+        return employeeRegistration;
     }
 }
