ffd6abf284fcd2aaa661d04f2740f82be0870b22
Igor Pasqualino <igorpasqualino@gmail.com>
2024-09-28 19:56:42 -0300

    Removed old api keys

--- FrontEnd/EurofarmaAdminFrontEnd/src/environments/environment.development.ts
+++ FrontEnd/EurofarmaAdminFrontEnd/src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
     eurofarmaComplianceApi: {
-        api_key: "zwt_O_UzAfwQJY6oP4mIyy7aCW2_KwOqL_7ohpGhGw",
-        corpus_key: "EurofarmaCompliance5657"
+        api_key: "",
+        corpus_key: ""
     },
     euroDataApi:{
-        api_key: "zwt_O_UzAfbWMUULhVseCd21psJnm--rE-JLCEo6VA",
-        corpus_key: "Eurodata"
+        api_key: "",
+        corpus_key: ""
     },
     backEndApiBase:{
         url: "http://localhost:8080"
